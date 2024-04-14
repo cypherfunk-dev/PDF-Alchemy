15f24fa05fc2804caf4b0e30f60d687481e4056c
Carlos <andrescarlos2211@gmail.com>
2024-04-14 17:34:04 -0400

    add ocr

---
+++ checkmark.py
@@ -0,0 +1,9 @@
+# import customtkinter
+# from CTkMessagebox import CTkMessagebox
+# import tkinter
+
+# def show_checkmark():
+#     CTkMessagebox(title="Operation Finished", message="Conversion completed successfully", icon="check", option_1="Ok")
+#     label.forget()
+#     imagenql.pack(pady=20, padx=20)
+#     print("Holamundo")
